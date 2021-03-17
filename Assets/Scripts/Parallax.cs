37dd75f7031896266c6305ea266900446fdff8f3
ReinerJasin <reinerjasin@gmail.com>
2021-03-17 22:31:53 +0800

    dikit lagi parallax

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startpos;
-    public GameObject cam;
-    public float parallaxEffect;
-    
+
+    float speed = 0.020f;
+    float batasKiri = -23.42f;
+    float batasKanan = 52.84f;
+    Vector3 translationVec;
     // Start is called before the first frame update
     void Start()
     {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        //float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        //float dist = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
-
-        if (temp > startpos + length)
+        //transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        if (transform.localPosition.x <= batasKiri)
         {
-            startpos += length;
+            transform.localPosition = new Vector3(batasKanan, transform.localPosition.y, transform.localPosition.z);
         }
-        else if (temp < startpos - length) {
-            startpos -= length;
-        }
+
+
+        translationVec = new Vector3(-3, 0, 0);
+
+        transform.localPosition += translationVec * speed;
     }
 }
