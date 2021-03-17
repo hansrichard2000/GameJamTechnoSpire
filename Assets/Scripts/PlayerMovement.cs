d73a94848dd29a086ced41b39863f4bb97bae512
ReinerJasin <reinerjasin@gmail.com>
2021-03-17 21:35:13 +0800

    bisa gerak atas bawah

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    float speed;
+    float batasAtas = 3.50f;
+    float batasBawah = -3.00f;
+    Vector3 translationVec;
+        
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (Input.GetKey(KeyCode.UpArrow)){
+                speed = 0.1f;
+        }
         
+        translationVec = new Vector3(0, 1, 0);
+
+        transform.localPosition += translationVec * speed;
+
+        if (transform.localPosition.y < batasBawah)
+        {
+            speed = 0f;
+        }
+        else
+        {
+            speed = -0.1f;
+        }
     }
 }
