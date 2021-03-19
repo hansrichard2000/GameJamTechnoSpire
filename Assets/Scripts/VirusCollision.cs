b5de3fb866643ac0a6d690a1fbab0ff16be5456a
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2021-03-19 22:28:06 +0700

    Modify and input assets

--- Assets/Scripts/VirusCollision.cs
+++ Assets/Scripts/VirusCollision.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class VirusCollision : MonoBehaviour
 {
+    public GameObject prefabExplosion;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +15,15 @@ public class VirusCollision : MonoBehaviour
     {
         if (collision.collider.tag == "Player")
         {
-            /*var explosion = Instantiate(prefabExplosion);
-            explosion.transform.position = this.transform.position;*/
+            var explosion = Instantiate(prefabExplosion);
+            explosion.transform.position = this.transform.position;
             Destroy(this.gameObject);
             Destroy(collision.collider.gameObject);
         }
         if (collision.collider.tag == "Bullet")
         {
-            /*var explosion = Instantiate(prefabExplosion);
-            explosion.transform.position = this.transform.position;*/
+            var explosion = Instantiate(prefabExplosion);
+            explosion.transform.position = this.transform.position;
             Destroy(this.gameObject);
             Destroy(collision.collider.gameObject);
         }
