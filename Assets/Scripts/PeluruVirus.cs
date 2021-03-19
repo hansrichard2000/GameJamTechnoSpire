c27a3420b9a65b3d380698dc6baffd35b663ae92
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2021-03-19 12:25:50 +0700

    Virus bisa nembak

---
+++ Assets/Scripts/PeluruVirus.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeluruVirus : MonoBehaviour
+{
+    Vector3 translationVec;
+    float speed = 0.05f;
+    float BATAS_KIRI = -10f;
+    float vers = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += translationVec * speed;
+
+        if(transform.localPosition.x < BATAS_KIRI) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void TembakDari(Vector3 posAwal, Vector3 direction)
+    {
+        transform.localPosition = new Vector3(posAwal.x, posAwal.y, posAwal.z);
+
+        translationVec = new Vector3(-Mathf.Cos(0), Mathf.Sin(0), 0);
+    }
+}
---
+++ Assets/Scripts/Virus2Behavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus2Behavior : MonoBehaviour
+{
+    private Vector3 translationVec;
+    public GameObject prefabPeluruMonster;
+    private float timer = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attack();    
+    }
+
+    private void Attack()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            var peluruBaru = Instantiate(prefabPeluruMonster);
+            peluruBaru.GetComponent<PeluruVirus>().TembakDari(transform.localPosition, -translationVec);
+            timer = 3f;
+        }
+    }
+}
