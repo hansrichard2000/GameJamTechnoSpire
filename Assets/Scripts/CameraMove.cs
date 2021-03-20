19374c13c32b3ffe18004f4d8af5f4033542af14
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2021-03-20 12:16:28 +0700

    Making Natural Parallax

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    Vector3 translationVec;
+    float speed = 0.020f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        translationVec = new Vector3(3, 0, 0);
+
+        transform.localPosition += translationVec * speed;
+    }
+}
--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
 
-    float speed = 0.020f;
+    private void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+    }
+
+    private void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if(temp > startpos + length)
+        {
+            startpos += length;
+            
+        }else if(temp < startpos - length)
+        {
+            startpos -= length;
+        }
+
+    }
+
+
+    /*float speed = 0.020f;
     float batasKiri = -23.42f;
     float batasKanan = 52.84f;
     Vector3 translationVec;
@@ -30,5 +59,5 @@ public class Parallax : MonoBehaviour
         translationVec = new Vector3(-3, 0, 0);
 
         transform.localPosition += translationVec * speed;
-    }
+    }*/
 }
