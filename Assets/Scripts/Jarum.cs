8e869ff2f6f239ebbcff970ad10980b364fca237
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2021-03-20 22:35:18 +0700

    Berhasil mengeluarkan jarum tapi tidak maju

--- Assets/Scripts/Jarum.cs
+++ Assets/Scripts/Jarum.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Jarum : MonoBehaviour
 {
+    float speed = 0.5f;
+    Vector3 translationVec;
+    float BATAS_KANAN = 10f;
+    //float BATAS_KIRI = -9f;
+    //float BATAS_ATAS = 5f;
+    //float BATAS_BAWAH = -5f;
+    public GameObject firePoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +19,22 @@ public class Jarum : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.localPosition += translationVec * speed;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "BatasKanan")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void TembakDari(GameObject posAwal, Vector3 direction)
+    {
+        translationVec = new Vector3(direction.x, direction.y, direction.z);
+    }
+
 }
---
+++ Assets/Scripts/PlayerShotgun.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShotgun : MonoBehaviour
+{
+    float speed;
+    float batasAtas = 3.50f;
+    float batasBawah = -3.00f;
+    Vector3 translationVec;
+    public GameObject prefabPeluru;
+    public GameObject firePoint;
+    public ScoringSystem scoringSystem;
+    public Animator animator;
+    public ParticleSystem particleSystem;
+
+    [SerializeField]
+    private Transform barrel;
+
+    [SerializeField]
+    private Rigidbody2D bullet;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystem.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            speed = 0.075f;
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            //entah kenapa play sama stop nya kebalik, kalau diplay, malah stop
+            particleSystem.Play();
+        }
+
+        translationVec = new Vector3(0, 1, 0);
+
+        transform.localPosition += translationVec * speed;
+
+        if (transform.localPosition.y < batasBawah)
+        {
+            speed = 0f;
+            animator.SetBool("Jump", false);
+        }
+        else
+        {
+            speed = -0.075f;
+            //particleSystem.Stop();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                var spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+
+                switch (i)
+                {
+                    case 0:
+                        spawnedBullet.AddForce(barrel.up * speed + new Vector3(0f, -90f, 0f));
+                        break;
+                    case 1:
+                        spawnedBullet.AddForce(barrel.up * speed + new Vector3(0f, 0f, 0f));
+                        break;
+                    case 2:
+                        spawnedBullet.AddForce(barrel.up * speed + new Vector3(0f, 90f, 0f));
+                        break;
+                }
+                /*spawnedBullet.GetComponent<Jarum>().TembakDari(firePoint, translationVec);
+                spawnedBullet.GetComponent<BulletDestroy>().scoringSystem = scoringSystem;*/
+            }
+
+            //Tembak
+            //Buat pelurunya
+
+            //set awal dan arah dari peluru
+            
+
+        }
+
+    }
+}
