53c6a81104c256679d3708ee327d4f5738ccf36a
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2021-03-22 01:27:00 +0700

    Hopely it's final

---
+++ Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToMenu : MonoBehaviour
+{
+    public GameObject credit;
+
+    public void OnPlayButton()
+    {
+        credit.SetActive(false);
+    }
+}
