766db5869e4fb1f1d8985b190bd2093f94fe5e67
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2021-03-21 12:33:01 +0700

    Fix asset for Game Over

--- Assets/Scripts/SoundSystem.cs
+++ Assets/Scripts/SoundSystem.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class SoundSystem : MonoBehaviour
 {
+    private static SoundSystem sound;
+
     // Start is called before the first frame update
     void Awake()
     {
         DontDestroyOnLoad(this);
+        if(sound == null)
+        {
+            sound = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
