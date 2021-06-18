07a0b96f0bee445de94dbcd3e2ea0ec119fb75a3
ReinerJasin <reinerjasin@gmail.com>
2021-06-18 12:46:20 +0800

    Pasang script hold di button

---
+++ Assets/JumpButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JumpButton : MonoBehaviour,IUpdateSelectedHandler,IPointerDownHandler,IPointerUpHandler
+{
+    public bool isPressed;
+    public GameObject playerMachineGun;
+    public PlayerMovement playerMovement;
+
+    // Start is called before the first frame update
+    public void OnUpdateSelected(BaseEventData data)
+    {
+        print("ooit");
+        if (isPressed)
+        {
+            playerMachineGun.GetComponent<PlayerMovement>().Jump();
+        }
+    }
+    public void OnPointerDown(PointerEventData data)
+    {
+        isPressed = true;
+    }
+    public void OnPointerUp(PointerEventData data)
+    {
+        isPressed = false;
+    }
+}
